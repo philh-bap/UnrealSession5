0c16dd513deb92006589c0211defab628e50ab59
philh-bap <philh@bap.jp>
2024-01-02 17:29:55 +0700

    Video 131

---
+++ Source/ToonTanks/Tower.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePawn.h"
+#include "Tank.h"
+#include "Tower.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API ATower : public ABasePawn
+{
+	GENERATED_BODY()
+public:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+private:
+	ATank* Tank;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Combat")
+	float FireRange = 300;
+};
